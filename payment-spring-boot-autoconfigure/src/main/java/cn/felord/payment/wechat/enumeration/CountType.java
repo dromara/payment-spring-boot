c101cdd8579601b85b6a69f502ad021e843375ca
dax <felord@qq.com>
2020-12-28 15:41:29 +0800

    refactor: 先享卡优化

---
+++ payment-spring-boot-autoconfigure/src/main/java/cn/felord/payment/wechat/enumeration/CountType.java
@@ -0,0 +1,36 @@
+/*
+ *
+ *  Copyright 2019-2020 felord.cn
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *  Website:
+ *       https://felord.cn
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package cn.felord.payment.wechat.enumeration;
+
+/**
+ * 优惠数量的类型标识
+ *
+ * @author felord.cn
+ * @since 1.0.3.RELEASE
+ */
+public enum CountType {
+    /**
+     * 不限数量
+     */
+    COUNT_UNLIMITED,
+    /**
+     * 有限数量
+     */
+    COUNT_LIMIT
+}
