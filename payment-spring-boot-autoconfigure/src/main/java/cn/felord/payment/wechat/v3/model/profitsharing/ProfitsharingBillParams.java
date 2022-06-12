d702d98363c74a078a89089a9c5f70d1cba1b865
Fang <felord@qq.com>
2022-06-12 15:19:28 +0800

    feat: 新增申请分账账单API

--- payment-spring-boot-autoconfigure/src/main/java/cn/felord/payment/wechat/v3/model/profitsharing/ProfitsharingBillParams.java
+++ payment-spring-boot-autoconfigure/src/main/java/cn/felord/payment/wechat/v3/model/profitsharing/ProfitsharingBillParams.java
@@ -1,5 +1,4 @@
 /*
- *
  *  Copyright 2019-2022 felord.cn
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,22 +14,23 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package cn.felord.payment.wechat.v3.model;
+
+package cn.felord.payment.wechat.v3.model.profitsharing;
 
 import cn.felord.payment.wechat.enumeration.TarType;
-import cn.felord.payment.wechat.enumeration.TradeBillType;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 /**
- * 申请交易账单请求参数
+ * 直连商户-申请分账账单API参数
  *
  * @author felord.cn
- * @since 1.0.3.RELEASE
+ * @since 1.0.12.RELEASE
  */
 @Data
-public class TradeBillParams {
+public class ProfitsharingBillParams {
+
     /**
      * 账单日期，必传。
      * <p>
@@ -38,33 +38,10 @@ public class TradeBillParams {
      */
     private LocalDate billDate;
     /**
-     * 二级商户号，选填。
-     *
-     * <ol>
-     *     <li>若商户是直连商户：无需填写该字段。</li>
-     *     <li>若商户是服务商：
-     *     <ul>
-     *         <li>不填则默认返回服务商下的交易或退款数据。</li>
-     *         <li>如需下载某个子商户下的交易或退款数据，则该字段必填。</li>
-     *     </ul>
-     *     </li>
-     * </ol>
-     * <p>
-     * 特殊规则：最小字符长度为8
-     * <p>
-     * 注意：仅适用于电商平台 服务商
-     */
-    private String subMchid;
-    /**
-     * 账单类型,不填则默认值为{@link TradeBillType#ALL}
-     *
-     * @see TradeBillType
-     */
-    private TradeBillType billType;
-    /**
      * 压缩类型，不填默认值为数据流
      *
      * @see TarType
      */
     private TarType tarType;
+
 }
